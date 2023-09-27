189ff775b2a4d64d0dfa2d7c29f00a442abdfa7d
TheBlckbird <theblckbrd@proton.me>
2023-09-27 11:04:57 +0200

    initial commit

---
+++ src/main/java/com/louisweigel/toggletoggle/ToggleToggle.java
@@ -0,0 +1,13 @@
+package com.louisweigel.toggletoggle;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ToggleToggle implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/louisweigel/toggletoggle/client/ToggleToggleClient.java
@@ -0,0 +1,13 @@
+package com.louisweigel.toggletoggle.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ToggleToggleClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
